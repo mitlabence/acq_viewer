3f109aa71729e91af7191ca2fb1aa14884f01de9
Bence Mitlasoczki <mitlabence111@gmail.com>
2024-09-27 11:15:32 +0200

    Added dialogs, scrolling only changes x axis (Ctl + scrolling y axis)

--- plot_acq_hdf5.py
+++ plot_acq_hdf5.py
@@ -3,7 +3,20 @@ import numpy
 import os
 import h5py
 import numpy as np
+from tkinter import Tk  # use tkinter to open files
+from tkinter.filedialog import askopenfilename, askdirectory
 
+def open_file(title: str = "Select file") -> str:
+    """
+    Opens a tkinter dialog to select a file. Returns the path of the file.
+    :param title: The message to display in the open directory dialog.
+    :return: the absolute path of the directory selected.
+    """
+    root = Tk()
+    # dialog should open on top. Only works for Windows?
+    root.attributes("-topmost", True)
+    root.withdraw()  # keep root window from appearing
+    return os.path.normpath(askopenfilename(title=title))
 
 class ZoomPan:
     def __init__(self):
@@ -16,15 +29,21 @@ class ZoomPan:
         self.y1 = None
         self.xpress = None
         self.ypress = None
+        self.ctl_pressed = False
 
     def zoom_factory(self, ax, base_scale=2.):
+        def onKeyPressed(event):
+            if event.key == "control":
+                self.ctl_pressed = True
+        def onKeyReleased(event):
+            if event.key == "control":
+                self.ctl_pressed = False
         def zoom(event):
             cur_xlim = ax.get_xlim()
             cur_ylim = ax.get_ylim()
 
             xdata = event.xdata  # get event x location
             ydata = event.ydata  # get event y location
-
             if event.button == 'down':
                 # deal with zoom in
                 scale_factor = 1 / base_scale
@@ -41,16 +60,18 @@ class ZoomPan:
 
             relx = (cur_xlim[1] - xdata)/(cur_xlim[1] - cur_xlim[0])
             rely = (cur_ylim[1] - ydata)/(cur_ylim[1] - cur_ylim[0])
-
-            ax.set_xlim([xdata - new_width * (1-relx),
-                        xdata + new_width * (relx)])
-            ax.set_ylim([ydata - new_height * (1-rely),
-                        ydata + new_height * (rely)])
+            if self.ctl_pressed:
+                ax.set_ylim([ydata - new_height * (1-rely),
+                            ydata + new_height * (rely)])            
+            else:
+                ax.set_xlim([xdata - new_width * (1-relx),
+                            xdata + new_width * (relx)])
             ax.figure.canvas.draw()
 
         fig = ax.get_figure()  # get the figure of interest
         fig.canvas.mpl_connect('scroll_event', zoom)
-
+        fig.canvas.mpl_connect("key_press_event", onKeyPressed)
+        fig.canvas.mpl_connect("key_release_event", onKeyReleased)
         return zoom
 
     def pan_factory(self, ax):
@@ -92,7 +113,7 @@ class ZoomPan:
 
 
 # convert .acq file with bioread CLI command acq2hdf5
-fpath_eeg = "acq-eeg-file-path.hdf5"
+fpath_eeg = open_file("Select EEG .hdf5 file to plot!")
 assert os.path.exists(fpath_eeg)
 
 with h5py.File(fpath_eeg, "r") as hf:
@@ -110,7 +131,7 @@ ax = fig.add_subplot(111, xlim=(  # plot 5 minutes = 360 s
     ts_eeg[0], ts_eeg[360*2000]), ylim=(np.min(eeg_data), np.max(eeg_data)), autoscale_on=False)
 
 
-ax.plot(ts_eeg, eeg_data)
+ax.plot(ts_eeg, eeg_data, linewidth=0.5)
 scale = 1.1
 zp = ZoomPan()
 figZoom = zp.zoom_factory(ax, base_scale=scale)
